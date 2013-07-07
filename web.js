93c3651f018524c2101417ccedbf0b2045a5a32c
annie guo <annie.guo.2011@gmail.com>
2013-07-07 22:04:49 +0000

    Update web.js to read index.html

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var txt = new Buffer(24);
+
+function readFileSync_encoding(filename, encoding) {
+    var buffer = fs.readFileSync(filename);
+    return buffer.toString('utf8');
+}
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  txt = readFileSync_encoding('index.html');
+  response.send(txt);
 });
 
 var port = process.env.PORT || 5000;
